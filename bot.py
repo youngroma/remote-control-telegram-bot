b31d6b1ddf101441c28874736b2e03384906f2b4
youngroma <krivichroma228@gmail.com>
2025-05-20 13:04:01 +0200

    Code reduce to DRY principle

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import ctypes
+import time
+
 import pyautogui
 import telebot
 from telebot import types
@@ -13,74 +15,52 @@ bot.send_message(config.CHAT_ID, "Bot online")
 
 @bot.message_handler(commands=["start", "help"])
 def start(message):
-	rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
-	btns = [["pause", "quiet", "loud", "<-", "->"]]
+    rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btns = [["pause", "quiet", "loud", "<-", "->"]]
 
-	if btns:  # Check that the list of btns is not empty
-		rmk.row(*btns[0])
-	bot.send_message(message.chat.id,
-		"""
-		Hi, this is a universal remote controller to help you manage your computer from your phone
-		
-		pause - pauses the video
-		quiet - makes the sound go quiet
-		loud - makes the sound go louder
-		<- - rewinds your video back 
-		-> - rewinds your video forward
-		 
-		write /start
-		""",
-		reply_markup=rmk
-	)
-
-
-@bot.message_handler(content_types=["text"])
-def commands_handler(message):
-
-	if message.text == "pause":
-		presspause(message)
-	elif message.text == "quiet":
-		quieter(message)
-	elif message.text == "loud":
-		loud(message)
-	elif message.text == "<-":
-		back(message)
-	elif message.text == "->":
-		forward(message)
+    if btns:  # Check that the list of btns is not empty
+        rmk.row(*btns[0])
+    bot.send_message(message.chat.id,
+        """
+        Hi, this is a universal remote controller to help you manage your computer from your phone
+        
+        pause - pauses the video
+        quiet - makes the sound go quiet
+        loud - makes the sound go louder
+        <- - rewinds your video back 
+        -> - rewinds your video forward
+         
+        write /start
+        """,
+        reply_markup=rmk
+    )
 
 
-@bot.message_handler(commands=["PAUSE", "pause"])
-def presspause(message):
-	keyboard.press('space')
-	bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+command_actions = {
+    "pause": lambda: keyboard.press('space'),
+    "quiet": lambda: keyboard.press_and_release('volume down'),
+    "loud": lambda: keyboard.press_and_release('volume up'),
+    "<-": lambda: keyboard.press_and_release('left arrow'),
+    "->": lambda: keyboard.press_and_release('right arrow')
+}
 
-@bot.message_handler(commands=["quiet"])
-def quieter(message):
-	keyboard.press_and_release('volume down')
-	bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-
-@bot.message_handler(commands=["loud"])
-def loud(message):
-	keyboard.press_and_release('volume up')
-	bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-
-@bot.message_handler(commands=["<-"])
-def back(message):
-	keyboard.press_and_release('left arrow')
-	bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-
-@bot.message_handler(commands=["->"])
-def forward(message):
-	keyboard.press_and_release('right arrow')
-	bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-
-
+@bot.message_handler(func=lambda m: m.text and m.text.lstrip('/').lower() in command_actions)
+def handle_command(message):
+    cmd = message.text.lstrip('/').lower()
+    action = command_actions.get(cmd)
+    if action:
+        action()
+    # Delete user message
+    try:
+        bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    except Exception:
+        bot.send_message(message.chat.id, "Something went wrong")
 
 def sms_to_client(message):
-	try:
-		pyautogui.alert(message.text, "Message")
-	except Exception:
-		bot.send_message(message.chat.id, "Something went wrong")
+    try:
+        pyautogui.alert(message.text, "Message")
+    except Exception:
+        bot.send_message(message.chat.id, "Something went wrong")
 
 
 # @bot.message_handler(commands=["input"])
@@ -101,9 +81,9 @@ def sms_to_client(message):
 # 	file = message.photo[-1].file_id
 # 	file = bot.get_file(file)
 
-	# download_file = bot.download_file(file.file_path)
-	# with open("image.jpg", "wb") as img:
-	# 	img.write(download_file)
-	#
-	# path = os.path.abspath("image.jpg")
-	# ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+    # download_file = bot.download_file(file.file_path)
+    # with open("image.jpg", "wb") as img:
+    # 	img.write(download_file)
+    #
+    # path = os.path.abspath("image.jpg")
+    # ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
